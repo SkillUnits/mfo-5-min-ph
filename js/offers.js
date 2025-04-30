778ee51799c7464085731f06f0b843d74845c01f
Rostyslav Hubariev <r.gubarev1303@gmail.com>
2025-04-30 19:44:52 +0300

    Initial commit

---
+++ js/offers.js
@@ -0,0 +1,94 @@
+class Offer {
+    constructor(logo, url, duration, interest) {
+        this.logo = logo;
+        this.url = url;
+        this.duration = duration;
+        this.interest = interest;
+    }
+}
+
+const moneySymbols = ["💵", "💰", "💸", "💳", "💶", "💷"];
+
+// Динамически загружаем только те оферы, которые есть в массиве offerIdsArray
+const offersData = {
+    10: new Offer(logo="/images/banks/zaimer.png", url="{offer}&offer_id=10", duration="12 months", interest="0.1%"),
+    12: new Offer(logo="/images/banks/moneymankz.svg", url="{offer}&offer_id=12", duration="30 days", interest="0.01%"),
+    13: new Offer(logo="/images/banks/onecreditkz.svg", url="{offer}&offer_id=13", duration="20 days", interest="0.01%"),
+    14: new Offer(logo="/images/banks/credity360.png", url="{offer}&offer_id=14", duration="30 days", interest="0.01%"),
+    15: new Offer(logo="/images/banks/creditbarkz.svg", url="{offer}&offer_id=15", duration="30 days", interest="0.1%"),
+    16: new Offer(logo="/images/banks/creditpluskz.svg", url="{offer}&offer_id=16", duration="30 days", interest="0.1%"),
+    20: new Offer(logo="/images/banks/acreditkz.png", url="{offer}&offer_id=20", duration="25 days", interest="0.01%"),
+    23: new Offer(logo="/images/banks/turbomoneykz.png", url="{offer}&offer_id=23", duration="20 days", interest="0.1%"),
+    24: new Offer(logo="/images/banks/gmoneykz.svg", url="{offer}&offer_id=24", duration="20 days", interest="0.29%"),
+
+    21: new Offer(logo="/images/banks/finlitekz.svg", url="{offer}&offer_id=21", duration="30 days", interest="0.01%"),
+    22: new Offer(logo="/images/banks/cashradarkz.svg", url="{offer}&offer_id=22", duration="30 days", interest="0.01%"),
+    25: new Offer(logo="/images/banks/easycashkz.svg", url="{offer}&offer_id=25", duration="60 days", interest="0.1%"),
+    26: new Offer(logo="/images/banks/excashkz.svg", url="{offer}&offer_id=26", duration="180 days", interest="0.1%"),
+    27: new Offer(logo="/images/banks/fincashkz.svg", url="{offer}&offer_id=27", duration="180 days", interest="0.1%"),
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Получаем список оферов из URL
+    const urlParams = new URLSearchParams(window.location.search);
+    var offersIds = urlParams.get("offers_ids");  // например, "zaimerkz,moneymankz,onecreditkz"
+    if (!offersIds) {
+        offersIds="13 21 22 20 10 12 14 16";
+    }
+
+    const offerIdsArray = offersIds.split(' ');
+    const offersWrapper = document.querySelector("#offers_list");
+    var idx = 0;
+    offerIdsArray.forEach(offerId => {
+        const offerData = offersData[offerId];
+        if (offerData) {
+            randomSymbol = moneySymbols[Math.floor(Math.random() * moneySymbols.length)];
+
+            if(idx == 0 || idx == 1) {
+                labelDiv = `<div class="label red">Best for you</div>`;
+            } else if(idx == 2) {
+                labelDiv = `<div class="label yellow">Fast delivery</div>`;
+            } else {
+                labelDiv = '';
+            }
+
+            if(idx == 0) {
+                color="#b8d8be"
+            } else {
+                color="#e5e5e5"
+            }
+
+            const offerElement = document.createElement("div");
+            offerElement.setAttribute("id", `offer-${offerId}`);
+            offerElement.classList.add("offer", "offer-block-new");
+            offerElement.style.backgroundColor = color;
+            offerElement.innerHTML = `
+            <div class="offer-key" style="display: none;">${offerId}</div>
+            ${labelDiv}
+            <a class="body-card-logo" href="${offerData.url}" onclick="return goUrl(this, '${offerId}');" target="_blank">
+                <img src="${offerData.logo}" class="offer-logo" alt="${offerId}">
+            </a>
+            <ul class="offer-info">
+                <li>
+                <span class="text">Term up to:</span>
+                <span class="bold">${offerData.duration}</span>
+                </li>
+                <li>
+                <span class="text">Rate:</span>
+                <span class="bold colored">${offerData.interest}</span>
+                </li>
+            </ul>
+            <div class="general_button_wrapper">
+                <div class="button">
+                <a class="btn-main" target="_blank" href="${offerData.url}" onclick="return goUrl(this, '${offerId}');">
+                    Get ${randomSymbol}
+                </a>
+                </div>
+            </div>
+            `;
+            offersWrapper.appendChild(offerElement);
+            
+            idx++;
+        }
+    });
+});
---
+++ js/sms.js
@@ -0,0 +1,255 @@
+// Define the messages array
+class Flow {
+    constructor(operMessages) {
+        this.operMessages = operMessages;
+    }
+}
+
+class OperMessage {
+    constructor(operMessage, userAnswers, action) {
+        this.operMessage = operMessage;
+        this.userAnswers = userAnswers;
+        this.action = action;
+    }
+}
+
+class Answer {
+    constructor(text, action, conversionStatus) {
+        this.text = text;
+        this.action = action;
+        this.conversionStatus = conversionStatus
+    }
+}
+
+class Action {
+    static actionChangeFlow = "changeFlow";
+    static showCurrentOffers = "showCurrentOffers";
+    static showOffers = "showOffers";
+    constructor(type, value) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+const haveCreditsFlow = new Flow([
+    new OperMessage("Select the companies where you had loans:", null, new Action(Action.showCurrentOffers, null)),
+    new OperMessage("What amount would you like to receive?", [
+        new Answer("Up to 100,000 PHP", null, "amount_select_100"),
+        new Answer("100,000 - 300,000 PHP", null, "amount_select_100-300"),
+        new Answer("More than 300,000 PHP 💰", null, "amount_select_300+"),
+    ], null),
+    new OperMessage("Great! I am performing automatic verification", null, null),
+    new OperMessage("🎉🎁💰 Your requested loan is pre-approved. It can be obtained from these organizations:", null, new Action(Action.showOffers, null))
+]);
+
+const noCreditsFlow = new Flow([
+    new OperMessage("What amount would you like to receive?", [
+        new Answer("Up to 100,000 PHP", null, "amount_select_100"),
+        new Answer("100,000 - 300,000 PHP", null, "amount_select_100-300"),
+        new Answer("More than 300,000 PHP 💰", null, "amount_select_300+"),
+    ], null),
+    new OperMessage("Great! I am performing automatic verification", null, null),
+    new OperMessage("🎉🎁💰 Your requested loan is pre-approved. It can be obtained from these organizations:", null, new Action(Action.showOffers, null))
+]);
+
+// Flows
+const mainFlow = new Flow([
+    new OperMessage("Good afternoon 👋", null, null),
+    new OperMessage("My name is Aruzhan, I will find the best microloan offer for you.", null, null),
+    new OperMessage("Have you ever had microloans?", [
+        new Answer("Yes", new Action(Action.actionChangeFlow, haveCreditsFlow), "have_credits"),
+        new Answer("No", new Action(Action.actionChangeFlow, noCreditsFlow), "no_credits"),
+    ], null),
+]);
+
+var currentFlow = mainFlow
+var currentMessageIndex = 0;
+var bottomChatViewId = 'offers-select';
+var userOffers = [];
+
+function selectOffer(element, offer) {
+    if (element.className == "offer-select") {
+        userOffers.push(offer);
+        element.className = "offer-select-selected";
+    } else {
+        removeItemOnce(userOffers, offer);
+        element.className = "offer-select";
+    }
+}
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+  
+
+function doneSelectOffer(element) {
+    keitaroConvertion("my_offers_select");
+    document.getElementById('select-offer-button').style.display = 'none';
+    proceedToNextMessage();
+}
+
+function scrollToBottom() {
+    document.getElementById("main").scrollTop = document.getElementById("main").scrollHeight;
+
+    // window.scroll({
+    //     top: document.body.scrollHeight,
+    //     behavior: 'smooth'  // Optional: Adds smooth scrolling
+    // });
+}
+
+function renderMessage(messageDiv) {
+    if (bottomChatViewId) {
+        document.getElementById("chat-container").insertBefore(messageDiv, document.getElementById(bottomChatViewId))
+    } else {
+        document.getElementById("chat-container").insertBefore(messageDiv, null)
+    }
+
+    scrollToBottom();
+}
+
+function displayMessage(content, sender) {
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message", sender);
+    messageDiv.textContent = content;
+    renderMessage(messageDiv);
+}
+
+function showOperatorMessage() {
+    const message = currentFlow.operMessages[currentMessageIndex];
+    if (message.operMessage) {
+        const operatorMessageDiv = document.createElement("div");
+        operatorMessageDiv.classList.add("message", "operator", "typing");
+
+        const circleDiv = document.createElement("div");
+        circleDiv.classList.add("circle");
+
+        const circleDiv1 = document.createElement("div");
+        circleDiv1.classList.add("circle");
+
+        const circleDiv2 = document.createElement("div");
+        circleDiv2.classList.add("circle");
+
+        operatorMessageDiv.appendChild(circleDiv)
+        operatorMessageDiv.appendChild(circleDiv1)
+        operatorMessageDiv.appendChild(circleDiv2)
+
+        // Disable buttons while the operator is typing
+        disableAnswerButtons();
+
+        renderMessage(operatorMessageDiv);
+        
+        setTimeout(() => {
+            // Show operator message
+            operatorMessageDiv.textContent = message.operMessage;
+            
+            if (message.userAnswers && message.userAnswers.length > 0) {
+                // Show answer buttons
+                displayAnswerButtons(message.userAnswers);
+                operatorMessageDiv.classList.remove("typing")
+                scrollToBottom();
+            } else if (message.action) {
+                // Handle custom action
+                handleAction(message.action)
+            } else {
+                // Show next message
+                operatorMessageDiv.classList.remove("typing");
+                proceedToNextMessage();
+                scrollToBottom();
+            }
+        }, Math.random() * 1000 + 1000); // random delay between 1500ms and 2500ms
+    }
+}
+
+function displayAnswerButtons(answers) {
+    const answerButtonsContainer = document.getElementById("answer-buttons");
+
+    answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.classList.add("answer-button");
+        button.textContent = answer.text;
+        button.onclick = () => handleUserResponse(answer);
+        answerButtonsContainer.appendChild(button);
+    });
+
+    // Enable buttons after operator's message is shown
+    enableAnswerButtons();
+}
+
+function handleUserResponse(response) {
+    displayMessage(response.text, "user");
+    keitaroConvertion(response.conversionStatus);
+
+    if (response.action) {
+        handleAction(response.action)
+        hideButtons()
+        return
+    }
+
+    proceedToNextMessage();
+    hideButtons()
+}
+
+function handleAction(action) {
+    switch (action.type) {
+        case Action.actionChangeFlow:
+            currentFlow = action.value;
+            currentMessageIndex = 0;
+            showOperatorMessage();
+            break;
+        case Action.showCurrentOffers:
+            showOffersSelect(true)
+            break;
+        case Action.showOffers:
+            showOffersResult(true)
+            break;
+    }
+}
+
+function hideButtons() {
+    const answerButtonsContainer = document.getElementById("answer-buttons");
+    while (answerButtonsContainer.firstChild) {
+        answerButtonsContainer.removeChild(answerButtonsContainer.firstChild);
+    }
+}
+
+function proceedToNextMessage() {
+    currentMessageIndex++;
+    if (currentMessageIndex < currentFlow.operMessages.length) {
+        // Show next flow message
+        setTimeout(showOperatorMessage, 500);
+    }
+}
+
+function showOffersSelect(show) {
+    if (show) {
+        bottomChatViewId = "offers-result";
+    }
+
+    document.getElementById("offers-select").style.display = show ? "block" : "none";
+    scrollToBottom();
+}
+
+function showOffersResult(show) {
+    for (const element of userOffers) {
+        const view = document.getElementById("offer-" + element);
+        if (view) {
+            view.style.display = "none";
+        }
+    }
+
+    document.getElementById("offers-result").style.display = show ? "block" : "none";
+}
+
+function disableAnswerButtons() {
+    const answerButtonsContainer = document.getElementById("answer-buttons");
+    answerButtonsContainer.classList.add('locked'); // Lock the entire button container
+}
+
+function enableAnswerButtons() {
+    const answerButtonsContainer = document.getElementById("answer-buttons");
+    answerButtonsContainer.classList.remove('locked'); // Unlock the entire button container
+}
